325027acd5252bc1654eeddc4f0bad476314a21d
Nermeta <56845892+Nermeta@users.noreply.github.com>
2023-01-25 13:48:12 -0700

    Added Redis Connectivity

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require ("express");
 const app = express ();
 const port = 3000;
 const bodyParser = require ('body-parser');
+const redis = require('redis');
+const redisClient = redis.createClient({url:"redis://127.0.0.1:6379"});
+const {v4: uuidv4} = require('uuid');
+
 
 app.use(bodyParser.json());
 
@@ -14,7 +18,7 @@ app.post('/login', (req, res) =>{
     const loginUser = req.body.userName;
     console.log('Login username: ' +loginUser);
     if (loginUser ="wgailey@byui.edu" && loginPassword =="1Toenermit!" ){
-        const loginToken == uuidv4();
+        const loginToken = uuidv4();
         res.send(loginToken);
     }
     else {
@@ -24,5 +28,6 @@ app.post('/login', (req, res) =>{
 });
 
 app.listen(port,()=>{
+    redisClient.connect();
     console.log("listening");
 });
