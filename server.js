f49dde9715dd78c28e6cc5572357e914b42e58c6
Nermeta <56845892+Nermeta@users.noreply.github.com>
2023-02-04 16:57:05 -0700

    Created cookies for Auth

--- server.js
+++ server.js
@@ -6,19 +6,42 @@ const bodyParser = require ('body-parser');
 const redis = require('redis');
 const redisClient = redis.createClient({url:"redis://127.0.0.1:6379"});
 const {v4: uuidv4} = require('uuid');
+app.use(bodyParser.json());
+app.use(express.static('public'));
 
 
 app.use(bodyParser.json());
+
+app.post('/rapidsteptest', async (req, res) =>{
+    const steps = req.body;
+    await redisClient.zAdd("Steps", steps, 0);
+    console.log("Steps", steps);
+    res.send('saved');
+})
 
 app.get("/", (req, res) => {
     res.send("Hello Wynter");
 });
 
-app.post('/login', (req, res) =>{
+app.get("/validate", async (req, res) =>{
+    const loginToken = req.cookies.stedicookie;
+    console.log("loginToken", loginToken);
+    const loginUser = await redisClient.hGet("TokenMap",loginToken);
+    res.send(loginUser);
+}
+)
+
+
+app.post('/login', async (req, res) =>{
     const loginUser = req.body.userName;
+    const loginPassword = req.body.password;
     console.log('Login username: ' +loginUser);
-    if (loginUser ="wgailey@byui.edu" && loginPassword =="1Toenermit!" ){
+    const correctPassword = await redisClient.hGet('UserMap', loginUser);
+
+    if (correctPassword==loginPassword){
         const loginToken = uuidv4();
+        await redisClient.hSet('TokenMap',loginToken,loginUser);
+        res.cookie('stedicookie',loginToken)
         res.send(loginToken);
     }
     else {
