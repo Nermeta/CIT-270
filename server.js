6c9224b4b3d02531097fb75fee3f885d95b8a510
Nermeta <56845892+Nermeta@users.noreply.github.com>
2023-02-18 12:28:16 -0700

    Added SSH Keys

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const bodyParser = require ('body-parser');
 const redis = require('redis');
 const redisClient = redis.createClient({url:"redis://127.0.0.1:6379"});
 const {v4: uuidv4} = require('uuid');
+const https = require('https');
+const fs = require('fs');
+
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
@@ -56,7 +59,7 @@ app.post('/login', async (req, res) =>{
     if (correctPassword==loginPassword){
         const loginToken = uuidv4();
         await redisClient.hSet('TokenMap',loginToken,loginUser);
-        res.cookie('stedicookie',loginToken);
+        res.cookie('stedicookie', loginToken);
         res.send(loginToken);
     }
     else {
@@ -65,7 +68,20 @@ app.post('/login', async (req, res) =>{
     }
 });
 
-app.listen(port,()=>{
+// app.listen(port,()=>{
+//     redisClient.connect();
+//     console.log("listening");
+// });
+
+https.createServer(
+    {
+        key: fs.readFileSync('./server.key'),
+        cert: fs.readFileSync('./server.cert'),
+        ca: fs.readFileSync('./chain.pem')
+
+    },
+    app
+).listen(port,() =>{
     redisClient.connect();
-    console.log("listening");
-});
+    console.log('Listening on port: ' + port);
+})
