135d731b868b08486434787eb3ffd909e6ee3b7e
Nermeta <56845892+Nermeta@users.noreply.github.com>
2023-03-01 16:22:21 -0700

    Added Certificate Encryption Paths

--- server.js
+++ server.js
@@ -75,9 +75,9 @@ app.post('/login', async (req, res) =>{
 
 https.createServer(
     {
-        key: fs.readFileSync('./server.key'),
-        cert: fs.readFileSync('./server.cert'),
-        ca: fs.readFileSync('./chain.pem')
+        key: fs.readFileSync('/etc/letsencrypt/live/wyntergailey.cit270.com/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/wyntergailey.cit270.com/cert.pem'),
+        ca: fs.readFileSync('/etc/letsencrypt/live/wyntergailey.cit270.com/fullchain.pem')
 
     },
     app
